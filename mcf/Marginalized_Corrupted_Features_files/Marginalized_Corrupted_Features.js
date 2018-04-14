d6a633921966dcfb954d7e2765dafe514dbfee98
lvdmaaten <lvdmaaten@gmail.com>
2018-04-14 13:59:07 -0400

    add MCF

---
+++ mcf/Marginalized_Corrupted_Features_files/Marginalized_Corrupted_Features.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20130311
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),reflection_0:new IWReflection({opacity:0.50,offset:2.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Marginalized_Corrupted_Features_files/Marginalized_Corrupted_FeaturesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
