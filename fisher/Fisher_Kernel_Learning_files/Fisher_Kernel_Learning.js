ead2b3b64a77fdbf82ad4b26f4e2884c3914d3a2
lvdmaaten <lvdmaaten@gmail.com>
2018-04-14 14:09:46 -0400

    add FKL and STE

---
+++ fisher/Fisher_Kernel_Learning_files/Fisher_Kernel_Learning.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20120208
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Fisher_Kernel_Learning_files/Fisher_Kernel_LearningMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
---
+++ ste/Stochastic_Triplet_Embedding_files/Stochastic_Triplet_Embedding.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20120714
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Stochastic_Triplet_Embedding_files/Stochastic_Triplet_EmbeddingMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
